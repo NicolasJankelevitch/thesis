75c927c698a19a831574c4b01431ce789f41e4d9
Nicolas Jankelevitch <nicolas.jankelevitch@student.kuleuven.be>
2020-12-01 13:02:45 +0100

    poging tot gitignore fixen

--- COBRAS_dont_know/clustering_algorithms/kmeans_fixed_representative.py
+++
@@ -1,9 +0,0 @@
-from clustering_algorithms.clustering_algorithms import ClusterAlgorithm
-
-
-class KmeansFixedRepresentative(ClusterAlgorithm):
-    def __init__(self, n_runs=10):
-        self.n_runs = n_runs
-
-    def cluster(self, data, indices, k, ml, cl, seed=None):
-        print("todo")
--- COBRAS_testing/evaluate_clusterings/evaluation/area_under_ari_curve_coprf_like.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import auc
-
-def get_area_under_ari_curve(aris):
-    print(aris)
-    return 1
-    # return auc(aris, range(len(aris)))
-
