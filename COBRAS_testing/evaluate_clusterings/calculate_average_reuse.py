c4ef4db574f07559f48a17ed25a741e7758db15f
Nicolas Jankelevitch <nicolas.jankelevitch@student.kuleuven.be>
2020-12-12 13:54:31 +0100

    cleanup calculate reuse

--- COBRAS_testing/evaluate_clusterings/calculate_average_reuse.py
+++ COBRAS_testing/evaluate_clusterings/calculate_average_reuse.py
@@ -22,7 +22,6 @@ def calculate_and_plot_average_reuse(test_names, comparison_name):
                     reuses = data[data.find("nr_reuses") + 11:]
                     reuses = reuses[:reuses.find(",")]
                     total_of_dataset = total_of_dataset + int(reuses)
-            # print("{} has {} reused constraints".format(dataset_name, str(total_of_dataset)))
             total_of_test = total_of_test + total_of_dataset
         ax.bar(str(counter), total_of_test, label=test_name)
         ax.text(counter - 0.33, total_of_test / 2, str(total_of_test))
@@ -31,15 +30,3 @@ def calculate_and_plot_average_reuse(test_names, comparison_name):
     output_file_name = os.path.join(FIGURE_DIR, comparison_name, "reuses.png")
     os.makedirs(os.path.dirname(output_file_name), exist_ok=True)
     plt.savefig(output_file_name, dpi=300, bbox_inches='tight')
-
-
-if __name__ == '__main__':
-    # string = "nr_reuses: 707, nr_tries: 32313"
-    # reuses = string[string.find("nr_reuses")+11:]
-    # reuses = reuses[:reuses.find(",")]
-    # tries = string[string.find("nr_tries")+10:]
-    # print(reuses)
-    # print(tries)
-    calculate_and_plot_average_reuse(["strat_1", "strat_1_with_standard_kmeans", "strat_2",
-                                      "strat_2_with_standard_kmeans", "basic_COBRAS", "strat_2_with_selection",
-                                      "strat_2_with_selection_and_standard_kmeans"], "test_reuse_graph")
